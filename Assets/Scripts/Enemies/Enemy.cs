1b9a18ff67f78667e62f06ce33d0236e9f02fa83
Ayrton Cuffaro <101194995+Tatoair@users.noreply.github.com>
2023-10-06 10:58:48 -0300

    Generalización + Knockback
    Correción de scripts (Generalización) + Ataques que apliquen knockback (prototipo)

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -9,7 +9,7 @@ public class Enemy : MonoBehaviour {
     [Header("General")]
     public string enemyName;
     public int enemyMaxHp;
-    private int enemyCurrentHp; 
+    protected int enemyCurrentHp; 
     public float enemySpeed;
 
     [Header("Attack")]
@@ -20,48 +20,44 @@ public class Enemy : MonoBehaviour {
     public float attackCasting; //Tiempo de ejecución de ataque
     public float attackDuration; //Tiempo de vida de la hitbox
     public GameObject attack1;
-    ///////////////////////////////
-    public bool melee; //Borrar, los ataques deben ser melee o a rango no todo el enemigo (esto para generalizar)
-    ///////////////////////////////
+    protected bool isDoingSomething;
 
     [Header("Life Bar")]
     public GameObject RellenoVida;
     public GameObject TextoVida;
 
-    private NavMeshAgent navMeshAgent;
-    private Transform player;
-    private float lastAttack = 0;
-    private float pathUpdateDelay = 0.2f;
-    private float pathUpdateDeadline;
-    private float escapeDistance = 2f;
+    protected NavMeshAgent navMeshAgent;
+    protected Transform player;
+    protected float lastAttack = 0;
+    protected float pathUpdateDelay = 0.2f;
+    protected float pathUpdateDeadline;
+    protected float escapeDistance = 2f;
 
-    private void Awake() {
+    protected void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    void Start()
+    protected void Start()
     {
         player = GameObject.Find("Jugador").transform;
         enemyCurrentHp = enemyMaxHp;
         navMeshAgent.speed = enemySpeed;
         navMeshAgent.stoppingDistance = attackRange;
         TextoVida.GetComponent<TextMeshProUGUI>().text = enemyMaxHp+"";
-        StartCoroutine(EnemyAI());
     }
 
-    private enum EnemyState
+    protected enum EnemyState
     {
         Idle,
         Chasing,
         Attacking,
         Repositioning
     }
-    private EnemyState currentState = EnemyState.Idle;
+    protected EnemyState currentState = EnemyState.Idle;
 
-    public virtual IEnumerator EnemyAI()
+    protected void Update() //Todo lo que estaba en EnemyAI (o algo así) ahora está en el update
     {
-        while (true)
-        {
+        if(!isDoingSomething){
             switch (currentState)
             {
                 case EnemyState.Idle: //Logica de cambio de estados
@@ -79,68 +75,78 @@ public class Enemy : MonoBehaviour {
                     break;
 
                 case EnemyState.Chasing: //Logica de Movimiento
-                    navMeshAgent.stoppingDistance = attackRange;
                     Move(player);
                     currentState = EnemyState.Idle;
                     break;
 
                 case EnemyState.Attacking: //Logica de Ataque
-                    Attack(attack1, !melee);
-                    yield return new WaitForSeconds(attackCasting);
-                    lastAttack = Time.time;
+                    StartCoroutine(Attack());
                     currentState = EnemyState.Idle;
                     break;
 
                 case EnemyState.Repositioning: //Logica de Escape (que hace despues de atacar)
-                    LookAtTarget(player);
-                    Vector3 directionToPlayer = player.position - transform.position;
-                    float distanceToPlayer = directionToPlayer.magnitude;
-
-                    if (distanceToPlayer <= 5 /*Aca iria el rango del jugador*/)
-                    {
-                        Vector3 escapeDirection = -directionToPlayer.normalized;
-                        Vector3 escapeDestination = transform.position + escapeDirection * escapeDistance;
-                        navMeshAgent.SetDestination(escapeDestination);
-                        navMeshAgent.stoppingDistance = 0;
-                    }
+                    Reposition();
                     currentState = EnemyState.Idle;
                     break;
             }
-            yield return null;
         }
     }
-    
-    void Update()
-    {
 
+    public virtual IEnumerator Attack() //Separado en una funcion para generalizar
+    {
+        SpawnAttack(attack1);
+        isDoingSomething = true;
+        yield return new WaitForSeconds(attackCasting);
+        lastAttack = Time.time;
+        isDoingSomething = false;
+    }
+
+    public virtual void Reposition() //Separado en una funcion para generalizar
+    {
+        LookAtTarget(player);
+        Vector3 directionToPlayer = player.position - transform.position;
+        float distanceToPlayer = directionToPlayer.magnitude;
+
+        if (distanceToPlayer <= 5 /*Aca iria el rango del jugador*/)
+        {
+            Vector3 escapeDirection = -directionToPlayer.normalized;
+            Vector3 escapeDestination = transform.position + escapeDirection * escapeDistance;
+            navMeshAgent.SetDestination(escapeDestination);
+            navMeshAgent.stoppingDistance = 0;
+        }
     }
 
     public virtual void Move(Transform target){
+        navMeshAgent.stoppingDistance = attackRange;
         if (Time.time >= pathUpdateDeadline) {
             pathUpdateDeadline = Time.time + pathUpdateDelay;
             navMeshAgent.SetDestination(target.position);
         }
     }
 
-    private void Attack(GameObject attackPrefab, bool isProjectile)
+    public void SpawnAttack(GameObject attackPrefab)
     {
         Vector3 attackSpawn = transform.position + transform.forward * transform.localScale.x;
         GameObject attackInstantiated = Instantiate(attackPrefab, attackSpawn, Quaternion.identity);
+
         attackInstantiated.transform.rotation = transform.rotation;
         EnemyAttack attackStats = attackInstantiated.GetComponent<EnemyAttack>();
     
         attackStats.damage = attackDamage;
         attackStats.lastingTime = attackDuration;
+        attackStats.setEnemy(transform);
         //attackStats.Uniform_ResizeAttack(attackSize);
 
-        if (isProjectile)
+        if (attackStats.isProjectile)
         {
             Rigidbody rb = attackInstantiated.GetComponent<Rigidbody>();
             rb.AddForce(transform.forward * attackSpeed, ForceMode.VelocityChange);
+        } else {
+            attackInstantiated.transform.parent = transform; //La hitbox ahora es hijo del enemigo para que esta lo siga
         }
     }
 
-    private void LookAtTarget (Transform target) {
+    protected void LookAtTarget (Transform target) {
         Vector3 lookPos = target.position - transform.position;
         lookPos.y = 0;
         Quaternion rotation = Quaternion.LookRotation(lookPos);
--- Assets/Scripts/Enemies/EnemyAttack.cs
+++ Assets/Scripts/Enemies/EnemyAttack.cs
@@ -6,28 +6,39 @@ public class EnemyAttack : MonoBehaviour
 {
     public int damage;
     public float lastingTime;
+    public bool isProjectile; //La hitbox corresponde a un disparo o no
+    public bool hasKnockback; //La hitbox causa retroceso al jugador
+    protected Transform enemy;
+    protected bool hasAppliedKnockback = false;
     
-    private IEnumerator LifeTime(float timeWhileAlive)
+    protected IEnumerator LifeTime(float timeWhileAlive)
     {
         yield return new WaitForSeconds(timeWhileAlive);
         Destroy(gameObject);
     }
     
-    void Start()
+    protected void Start()
     {   
         //Uniform_ResizeAttack(size);
         StartCoroutine(LifeTime(lastingTime));
-
     }
     
-    void OnTriggerEnter(Collider other)
+    protected void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
-            other.gameObject.GetComponent<Jugador>().RecibirDanio(damage);
+            Jugador player = other.gameObject.GetComponent<Jugador>();
+            player.RecibirDanio(damage);
+            if (hasKnockback && !hasAppliedKnockback) { //Aplicar knockback al jugador
+                player.ApplyKnockback(enemy);
+                hasAppliedKnockback = true;
+            }
         }
     }
-    
+
+    public void setEnemy (Transform position) { //Para saber donde esta el enemigo que lo creo (soluciona bugs del knockback)
+        enemy = position;
+    }
     
     public void Uniform_ResizeAttack(float sizeVar)
     {
